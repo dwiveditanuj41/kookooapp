1996202eaabbf284b641880e846bafa2899b91c8
dwiveditanuj41 <tanuj.dwivedi.civ16@itbhu.ac.in>
2019-04-05 05:17:23 +0530

    dc

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express();
 app.get('/', function(req, res){
 	
 	res.sendFile(__dirname + "/public/index.xml");
+	console.log(req);
+	console.log(res);
 	
 })
 
@@ -17,4 +19,3 @@ var server = app.listen(process.env.PORT || 3000, function(){
 	console.log("PORT is running on 3000");
 });
 
-app.get('GotDTMF', funtion(req,res){console.log(req); console.log(res);})
