5e5d8460d8ead354793d7b67535ecb560a44a3c8
dwiveditanuj41 <tanuj.dwivedi.civ16@itbhu.ac.in>
2019-04-05 02:28:07 +0530

    sdfjoa

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static("public"));
 app.get('/', function(req, res){
 	
-	res.sendFile(__dirname + "index.html");
+	res.sendFile(__dirname + "index");
 })
 
 var server = app.listen(process.env.PORT || 3000, function(){
