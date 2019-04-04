b21a293df1842587e8255a1ba5952437c460314c
dwiveditanuj41 <tanuj.dwivedi.civ16@itbhu.ac.in>
2019-04-05 04:31:39 +0530

    gfgfva

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var https = require('https');
 
 var app = express();
 
@@ -6,6 +7,8 @@ app.get('/', function(req, res){
 	
 	res.sendFile(__dirname + "/public/index.xml");
 })
+
+https.get('https://desolate-castle-29966.herokuapp.com/' , (resp) =>{console.log(resp)})
 
 var server = app.listen(process.env.PORT || 3000, function(){
 	console.log("PORT is running on 3000");
