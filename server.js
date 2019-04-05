43b928cac27e47acd7caedda179aa2ef87551399
dwiveditanuj41 <tanuj.dwivedi.civ16@itbhu.ac.in>
2019-04-06 05:28:50 +0530

    sda

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var http = require('http');
+var http = require('https');
 var events = require('events');
 var eventEmmiter = new events.EventEmitter();
 
@@ -11,7 +11,13 @@ app.get('/', function(req, res){
 	
 })
 
-
+http.get('https://desolate-castle-29966.herokuapp.com/?event=GotDTMF&data=1', (resp) => {
+  app.get('/', function(req, res){
+	
+	res.sendFile(__dirname + "/public/GotDTMF1.xml");
+	
+})
+  });
 
 
 
